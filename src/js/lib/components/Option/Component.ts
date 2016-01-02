bb223e6ccfa645486785128bcb3cdb992ae20b49
Alexander Trefz <alexander.trefz@gmail.com>
2016-01-02 23:59:32 +0100

    Implement value for Option

--- src/js/lib/components/Option/Component.ts
+++ src/js/lib/components/Option/Component.ts
@@ -6,7 +6,7 @@ import OptionController from './Controller'
 
 export default class Option extends knife.Component {
 	initialize(options) {
-		this.model      = new OptionModel({ text: options.text }, this)
+		this.model      = new OptionModel({ text: options.text, value: options.value }, this)
 		this.view       = new OptionView(Object.assign({}, options, { model: this.model }), this)
 		this.controller = new OptionController({}, this)
 	}
--- src/js/lib/components/Option/Model.ts
+++ src/js/lib/components/Option/Model.ts
@@ -3,7 +3,8 @@ import * as knife from '../../vendor/knife/knife'
 export default class OptionModel extends knife.Model {
 	_setDefaultData(options) {
 		this.setMany({
-			text: options.text || ''
+			text: options.text || '',
+			value: options.value || null,
 		})
 	}
 }
